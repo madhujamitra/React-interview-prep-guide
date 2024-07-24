b77f64da06e5d460760b4509dddacaab14908ad7
madhujamitra <44411291+madhujamitra@users.noreply.github.com>
2024-07-24 16:42:00 -0700

    logic done

--- timmer/src/pages/Timer.jsx
+++ timmer/src/pages/Timer.jsx
@@ -1,26 +1,46 @@
-import React, { useState,useRef, useEffect } from "react"
+import React, { useState, useRef, useEffect } from "react";
 
-export default function Timer(){
-const[startTime, setStartTime] = useState(0)
-const[stopTime , setStopTime] = useState(false)
-const[current , setCurrent] = useState(0)
-let intervalRef = useRef();
+export default function Timer() {
+  const [timmer, setTimmer] = useState(false);
+  const [current, setCurrent] = useState(0);
+  let intervalId = useRef(null);
 
+  const hour = Math.floor(current/3600)
+  const min = Math.floor((current% 3600)/60)
+  const sec = current % 60
 
-const timmerStart= () =>{
-setStopTime(!stopTime)
-}
+  const timmerStart = () => {
+    if (timmer === false) {
+      intervalId.current = setInterval(() => {
+        setCurrent((pre) => pre + 1);
+      }, 1000);
+      setTimmer(true);
+    } else {
+      clearInterval(intervalId.current);
+      setTimmer(false);
+    }
+  };
 
+  const resetTimmer = () => {
+    clearInterval(intervalId.current);
+    setCurrent(0);
+    setTimmer(false);
+  };
 
+  
 
-    return (
-        <>
-        <div>
-            <div>{current}</div>
-            <button onClick={timmerStart}>{stopTime ? "Start the timmer" : "Stop the timmer"}</button>
-            
-        </div>
-        
-        </>
-    )
+  useEffect(() => {
+    return () => clearInterval(intervalId.current);
+  }, []);
+  return (
+    <>
+      <div>
+        <div>{hour}:{min}:{sec}</div>
+        <button onClick={timmerStart}>
+          {timmer ? "Start the timmer" : "Stop the timmer"}
+        </button>
+        <button onClick={resetTimmer}>reset timmer</button>
+      </div>
+    </>
+  );
 }
