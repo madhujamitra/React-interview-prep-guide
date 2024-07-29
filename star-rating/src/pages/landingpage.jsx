686b0e1ffe63af01d6bf617c3fa51dd2373781f7
madhujamitra <44411291+madhujamitra@users.noreply.github.com>
2024-07-29 15:30:00 -0700

    logic

--- star-rating/src/pages/landingpage.jsx
+++ star-rating/src/pages/landingpage.jsx
@@ -1,13 +1,30 @@
+import { useState } from "react";
+import { EmptyStar, FilledStar } from "../components/stars";
 
+export default function Landingpage() {
 
-import { EmptyStar, FilledStar } from '../components/stars';
+    
+  const [rating, setRating] = useState(0);
+  const [hover, setHover] = useState(0);
 
-export default function Landingpage(){
-    return (
-        <>
-        <EmptyStar/>
-        <FilledStar/>
-        </>
-    )
+  return (
+    <div>
+    {Array(5)
+      .fill(0)
+      .map((_, index) => {
+        const starValue = index + 1;
+        return (
+          <span
+            key={index}
+            onClick={() => setRating(starValue)}
+            onMouseEnter={() => setHover(starValue)}
+            onMouseLeave={() => setHover(0)}
+            style={{ cursor: "pointer" }}
+          >
+            {starValue <= (hover || rating) ? <FilledStar /> : <EmptyStar />}
+          </span>
+        );
+      })}
+  </div>
+  );
 }
-
